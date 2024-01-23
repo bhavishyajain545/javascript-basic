2e9c1a113cb5cec3091100e15c287653dfefe862
bhavishyajain545 <65864699+bhavishyajain545@users.noreply.github.com>
2024-01-23 08:35:39 +0000

    datatypes discussed

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)//we are using nodejs,not browser
+let name = "bhavishya"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null -> standalone value
+//symbol =>unique
+
+//object
+console.log(typeof "hite");//undefined
+console.log(typeof null);//object
+
+
