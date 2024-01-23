3d2b4a78cdda195b9a0a67a01dd4d5b86018f798
bhavishyajain545 <65864699+bhavishyajain545@users.noreply.github.com>
2024-01-23 08:13:46 +0000

    basic variables knowledge

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const schoolNo= 35555
+let schoolmail = "jainbahvishya545@gmail.com"
+var schoolname = "navodaya"
+accountcity = "Jaipur"
+ 
+console.table([schoolNo,schoolmail,accountcity,schoolname])
